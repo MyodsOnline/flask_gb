b5257a30c7cd2be66fa0262e27b76b0841a89243
diver <diver.vlz@gmail.com>
2023-10-05 23:31:44 +0300

    end of the day

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect
+from werkzeug.exceptions import NotFound
 
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
@@ -13,5 +14,7 @@ def user_list():
 @user.route('/<int:pk>', endpoint='detail')
 def get_user(pk):
     from blog.models import User
-    user = User.query.filter_by(id=pk).one_or_none()
-    return render_template('users/detail.html', user=user)
+    _user = User.query.filter_by(id=pk).one_or_none()
+    if not _user:
+        raise NotFound(f'User #{pk} not found')
+    return render_template('users/detail.html', user=_user)
