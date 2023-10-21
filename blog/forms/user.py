384dd0fddafadbb83012de707dc94d6331396c6e
diver <diver.vlz@gmail.com>
2023-10-21 16:09:32 +0300

    registration fom creation continue

--- blog/forms/user.py
+++ blog/forms/user.py
@@ -1,11 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, PasswordField, SubmitField
+from wtforms import StringField, validators, PasswordField, SubmitField, EmailField
+from wtforms.validators import InputRequired, Email
 
 
 class UserRegistrationForm(FlaskForm):
     username = StringField('Username', [validators.DataRequired()])
     first_name = StringField('First name', [validators.DataRequired()])
-    email = StringField('Email', [validators.DataRequired(), validators.Email()])
-    password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm_password')])
+    email = EmailField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField(
+        'Password', [
+            validators.DataRequired(),
+            validators.EqualTo('confirm_password', message='Passwords are not equal')
+        ])
     confirm_password = PasswordField('Confirm password', [validators.DataRequired()])
     submit = SubmitField('Register')
--- blog/user/views.py
+++ blog/user/views.py
@@ -1,19 +1,38 @@
-from flask import Blueprint, render_template, redirect, request
+from flask import Blueprint, render_template, redirect, request, url_for
 from werkzeug.exceptions import NotFound
-from flask_login import login_required
+from flask_login import login_required, login_user
+from werkzeug.security import generate_password_hash
+
 
 from blog.models import User
 from blog.forms.user import UserRegistrationForm
+from blog.extensions import db
 
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
 
-@user.route('/register', methods=['GET', 'POST'])
+@user.route('/register', methods=['GET', 'POST'], endpoint='register')
 def register():
     form = UserRegistrationForm(request.form)
     errors = []
+
     if request.method == 'POST' and form.validate_on_submit():
-        pass
+        if User.query.filter_by(email=form.email.data).count():
+            form.email.errors.append('email is not unique!')
+            render_template('users/register.html', form=form, errors=errors)
+
+        _user = User(
+            username = form.username.data,
+            first_name = form.first_name.data,
+            email = form.email.data,
+            password = generate_password_hash(form.password.data),
+        )
+
+        db.session.add(_user)
+        db.session.commit()
+
+        login_user(_user)
+        return redirect(url_for('user.user_list'))
 
     return render_template('users/register.html',
                            form=form,
@@ -21,7 +40,7 @@ def register():
                            )
 
 
-@user.route('/')
+@user.route('/', endpoint='user_list')
 def user_list():
     users = User.query.all()
     return render_template('users/users.html', users=users)
